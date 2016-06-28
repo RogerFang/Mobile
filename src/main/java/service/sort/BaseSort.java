15eeb80c097e5165e3a0f51c4e4aa3f5cec69d3d
Roger <1223493354@qq.com>
2016-06-28 21:05:22 +0800

    Create Mobile Process

---
+++ src/main/java/service/sort/BaseSort.java
@@ -0,0 +1,9 @@
+package service.sort;
+
+/**
+ * Created by Roger on 2016/6/28.
+ */
+public class BaseSort {
+
+
+}
---
+++ src/test/java/Test2.java
@@ -0,0 +1,81 @@
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+/**
+ * Created by Roger on 2016/6/28.
+ */
+public class Test2 {
+
+    @Test
+    public void test() throws FileNotFoundException {
+        int bufSize = 500;
+        File fin = new File("test.txt");
+        File fout = new File("test2.txt");
+
+        FileChannel fcin = new RandomAccessFile(fin, "r").getChannel();
+        ByteBuffer rBuffer = ByteBuffer.allocate(bufSize);
+
+        FileChannel fcout = new RandomAccessFile(fout, "rws").getChannel();
+        ByteBuffer wBuffer = ByteBuffer.allocateDirect(bufSize);
+
+        readFileByLine(bufSize, fcin, rBuffer, fcout, wBuffer);
+    }
+
+    public static void readFileByLine(int bufSize, FileChannel fcin, ByteBuffer rBuffer, FileChannel fcout, ByteBuffer wBuffer){
+        String enterStr = "\n";
+        try{
+            byte[] bs = new byte[bufSize];
+
+            int size = 0;
+            StringBuffer strBuf = new StringBuffer("");
+            while(fcin.read(rBuffer) != -1){
+                int rSize = rBuffer.position();
+                rBuffer.rewind();
+                rBuffer.get(bs);
+                rBuffer.clear();
+                String tempString = new String(bs, 0, rSize);
+                // System.out.print(tempString);
+                System.out.println(tempString.indexOf(enterStr));
+
+                int fromIndex = 0;
+                int endIndex = 0;
+                while((endIndex = tempString.indexOf(enterStr, fromIndex)) != -1){
+                    String line = tempString.substring(fromIndex, endIndex);
+                    line = new String(strBuf.toString() + line);
+                    // System.out.print(line);
+                    // writeFileByLine(fcout, wBuffer, line);
+
+
+                    strBuf.delete(0, strBuf.length());
+                    fromIndex = endIndex + 1;
+                }
+                if(rSize > tempString.length()){
+                    strBuf.append(tempString.substring(fromIndex, tempString.length()));
+                }else{
+                    strBuf.append(tempString.substring(fromIndex, rSize));
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void writeFileByLine(FileChannel fcout, ByteBuffer wBuffer, String line){
+        try {
+//write on file head
+//fcout.write(wBuffer.wrap(line.getBytes()));
+//wirte append file on foot
+            fcout.write(wBuffer.wrap(line.getBytes()), fcout.size());
+
+        } catch (IOException e) {
+// TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
